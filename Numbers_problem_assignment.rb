d938487abf9f693976d6606579e774888e382425
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-08-10 14:19:30 -0700

    Numbers problem assignment

---
+++ Numbers_problem_assignment.rb
@@ -0,0 +1,30 @@
+
+#Do the following three times
+3.times do 
+
+#Ask the user to input a positive integer value 
+  puts "Enter positive number " + (i+1).to_s +  ":"
+    i +=1
+    n = gets.chomp
+
+#Convert the user input to an integer and save the value in a variable.
+    num = n.to_i
+    x = num
+  
+  end
+end
+
+
+#Add 20 to each of the three numbers and print this new value for each to the console. 
+3.times do
+  puts "Twenty added to the numbers you entered gives us" + (i).to_s + ":"
+  your_number = rand(x)
+  output << your_number.to_i
+
+  puts output + 20
+
+  end
+end
+
+
+
